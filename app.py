bdfa4cdf3ce03a30daadf708506d3a4f580f73d6
Choeng Kimlay <mingfongmen@gmail.com>
2025-01-21 08:12:52 +0700

    Ádded Model File, Predict Price with Each Model, Not Yet Return Predicted Prices

--- app.py
+++ app.py
@@ -4,10 +4,22 @@ import joblib
 import pandas as pd
 import numpy as np
 from waitress import serve
+from sklearn.preprocessing import StandardScaler
 
 # Load the trained model and scaler from the files
-model = joblib.load('nural_network2.pkl')  # Load the model from 'nural_network2.pkl'
+# model = joblib.load('nural_network2.pkl')  # Load the model from 'nural_network2.pkl'
 # scaler = joblib.load('scaler.pkl')  # Load the scaler from 'scaler.pkl'
+models = {
+    'Neural Network'    : joblib.load('./model/Neural-Network-Model.pkl'),
+    'Random Forrest'    : joblib.load('./model/RFR-Model.pkl'),
+    'Decision Tree'     : joblib.load('./model/DT-Model.pkl'),
+    'Gradient Boosting' : joblib.load('./model/GB-Model.pkl'),
+    'LightGBM'          : joblib.load('./model/LGB-Model.pkl'),
+    'XGBoost'           : joblib.load('./model/XGB-Model.pkl'),
+    'Elastic Net'       : joblib.load('./model/ENR-Model.pkl'),
+    'Ridge'             : joblib.load('./model/Ridge-Model.pkl'),
+    'Lasso'             : joblib.load('./model/Lasso-Model.pkl')
+}
 
 # Initialize Flask app
 app = flask.Flask(__name__)
@@ -52,24 +64,38 @@ def predict():
         # Ensure all columns that the model expects are present
         real_df = real_df.reindex(columns=columns_list, fill_value=0)
         real_df = real_df.drop(columns=['Price'], errors='ignore')
+        real_df_scaled = StandardScaler().transform(real_df)
 
+        # # Assuming you have the lambda used for the Box-Cox transformation during training
+        price_lambda = -0.02832931484315432
+
+        # Initialize a DataFrame to store results
+        results = pd.DataFrame()
 
         # Print the shape to check if it's still empty
         print("Shape of real_df after reindexing:", real_df.shape)
 
         # Proceed if there are rows in real_df
         if real_df.shape[0] > 0:
-            # Scale the transformed data using the scaler
-            predicted_price = model.predict(real_df)
-            print(predicted_price)
+            # # Scale the transformed data using the scaler
+            # predicted_price = model.predict(real_df_scaled)
+            
+            # print(predicted_price)
 
-            # Assuming you have the lambda used for the Box-Cox transformation during training
-            price_lambda = -0.02832931484315432
-            # Convert the predicted price back to the original scale
-            predicted_price_original = inverse_boxcox(predicted_price.flatten(), price_lambda)
+            # # Convert the predicted price back to the original scale
+            # predicted_price_original = inverse_boxcox(predicted_price.flatten(), price_lambda)
+
+            # Predict prices for each model
+            for model_name, model in models.items():
+                predicted_price = model.predict(real_df_scaled)
+                predicted_price_original = inverse_boxcox(predicted_price.flatten(), price_lambda)
+                results[model_name] = [f"${predicted:,.2f}" for predicted in predicted_price_original]
+
+            # Display the results
+            print(results)
 
             # Output the predicted price
-            return jsonify({'predicted_price': str(predicted_price_original[0])})
+            return jsonify({'predicted_price'})
         else:
             return jsonify({'error': 'The DataFrame is empty, please check your data.'})
 
